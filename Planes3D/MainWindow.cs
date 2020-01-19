a4024fcdf52027c0b7df660ae0fc8ace6426db41
pflis <pawel.flis98@gmail.com>
2020-01-19 15:22:11 +0100

    Initial commit

---
+++ Planes3D/MainWindow.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assimp;
+using Assimp.Configs;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Input;
+using PrimitiveType = OpenTK.Graphics.OpenGL4.PrimitiveType;
+
+//using Assimp;
+
+namespace Planes3D
+{
+    public class MainWindow : GameWindow
+    {
+        private Shader _shader;
+       
+        private Dictionary<string, Model> models = new Dictionary<string, Model>();
+
+        private Model teapot;
+
+        private Matrix4 projection;
+        private Matrix4 view;
+
+        private readonly Vector3 _lightPos = new Vector3(1.2f, 1.0f, 2.0f);
+
+        public MainWindow(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { }
+
+        
+        protected override void OnLoad(EventArgs e)
+        {
+            models["sun"] = ModelLoader.LoadFromFile("../../models/sphere.obj", 
+                PostProcessSteps.Triangulate, 
+                null);
+
+            models["teapot"] = (ModelLoader.LoadFromFile("../../models/teapot.obj",
+                PostProcessSteps.Triangulate,
+                null));
+
+            _shader = new Shader("../../shaders/shader.vert", "../../shaders/shader.frag");
+            _shader.Use();
+
+            models["teapot"].matrix = Matrix4.Identity;
+            models["teapot"].texture = new Texture("../../textures/bricks.jpg");
+
+            models["sun"].matrix = Matrix4.Identity* Matrix4.CreateScale(0.3f);
+            models["sun"].texture = new Texture("../../textures/sun.jpg");
+
+            view = Matrix4.CreateTranslation(0.0f, 0.0f, -5.0f);
+            projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45f),
+                Width / Height,
+                0.1f,
+                100.0f);
+
+            GL.Enable(EnableCap.DepthTest);
+
+            base.OnLoad(e);
+        }
+
+        double _time = 0.0d;
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            _time += 10*e.Time;
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            foreach (var model in models)
+            {
+                var m = model.Value;
+                m.texture.Use();
+                _shader.Use();
+                var mm = m.matrix;
+
+                if(model.Key == "sun")
+                {
+                    mm *= Matrix4.CreateTranslation(_lightPos);
+                }
+
+                _shader.SetMatrix4("model", mm);
+                _shader.SetMatrix4("view", view);
+                _shader.SetMatrix4("projection", projection);
+                foreach (var mesh in m.meshes)
+                {
+                    mesh.Render();
+                }
+            }
+            
+            SwapBuffers();
+
+            base.OnRenderFrame(e);
+        }
+
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            var input = Keyboard.GetState();
+
+            models["teapot"].matrix = models["teapot"].matrix* Matrix4.CreateRotationY((float)MathHelper.DegreesToRadians(1f)) * Matrix4.CreateRotationZ((float)MathHelper.DegreesToRadians(0.23f)); ;
+
+            models["sun"].matrix = models["sun"].matrix * Matrix4.CreateRotationY((float)MathHelper.DegreesToRadians(-0.3f)) * Matrix4.CreateRotationZ((float)MathHelper.DegreesToRadians(0.1f)); ;
+
+            if (input.IsKeyDown(Key.Escape))
+            {
+                Exit();
+            }
+
+            base.OnUpdateFrame(e);
+        }
+
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45f),
+               Width / (float) Height,
+               0.1f,
+               100.0f);
+            base.OnResize(e);
+        }
+
+
+        protected override void OnUnload(EventArgs e)
+        {
+            // GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            // GL.BindVertexArray(0);
+            // GL.UseProgram(0);
+            //
+            // GL.DeleteBuffer(_vertexBufferObject);
+            // GL.DeleteVertexArray(_vertexArrayObject);
+            //
+            // GL.DeleteProgram(_shader.Handle);
+            // // Don't forget to dispose of the texture too!
+            // GL.DeleteTexture(_texture.Handle);
+            // base.OnUnload(e);
+        }
+    }
+}
---
+++ Planes3D/ModelLoader.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using OpenTK;
+using Assimp;
+using Assimp.Configs;
+using OpenTK.Graphics.OpenGL4;
+using PrimitiveType = OpenTK.Graphics.OpenGL4.PrimitiveType;
+
+namespace Planes3D
+{
+    public class Model
+    {
+        public Matrix4 matrix;
+        public Texture texture;
+        public List<Mesh> meshes = new List<Mesh>();
+    }
+
+    public class Mesh
+    {
+        public List<int> vboIdList = new List<int>(1000);
+        int vertexCount;
+        int vaoId;
+        float boundingRadius;
+
+
+        public void Init()
+        {
+            GL.BindVertexArray(vaoId);
+        }
+
+        public void End()
+        {
+            GL.BindVertexArray(0);
+        }
+
+        public void Render()
+        {
+            Init();
+
+            GL.DrawElements(PrimitiveType.Triangles, vertexCount, DrawElementsType.UnsignedInt, 0);
+
+            End();
+        }
+
+        public Mesh(List<float> positions, List<float> textCoords, List<float> normals, List<int> indices)
+        {
+            //vboIdList = new List<int>();
+         
+            vertexCount = indices.Count;
+
+            vaoId = GL.GenVertexArray();
+
+            GL.BindVertexArray(vaoId);
+
+            int vboId = GL.GenBuffer();
+            vboIdList.Add(vboId);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vboId);
+            GL.BufferData(BufferTarget.ArrayBuffer, 
+                positions.Count * sizeof(float), 
+                positions.ToArray(), 
+                BufferUsageHint.StaticDraw);
+            GL.EnableVertexAttribArray(0);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
+
+
+            vboId = GL.GenBuffer();
+            vboIdList.Add(vboId);
+
+
+        
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vboId);
+            GL.BufferData(BufferTarget.ArrayBuffer,
+                textCoords.Count * sizeof(float),
+                textCoords.ToArray(),
+                BufferUsageHint.StaticDraw);
+            GL.EnableVertexAttribArray(1);
+            GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, 0, 0);
+
+
+            vboId = GL.GenBuffer();
+            vboIdList.Add(vboId);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vboId);
+            GL.BufferData(BufferTarget.ArrayBuffer,
+                normals.Count * sizeof(float),
+                normals.ToArray(),
+                BufferUsageHint.StaticDraw);
+            GL.EnableVertexAttribArray(2);
+            GL.VertexAttribPointer(2, 3, VertexAttribPointerType.Float, false, 0, 0);
+
+
+            vboId = GL.GenBuffer();
+            vboIdList.Add(vboId);
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, vboId);
+            GL.BufferData(BufferTarget.ElementArrayBuffer,
+                indices.Count * sizeof(int),
+                indices.ToArray(),
+                BufferUsageHint.StaticDraw);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+
+        }
+
+        public static void processVertices(Assimp.Mesh mesh, List<float> vertices)
+        {
+            foreach (var v in mesh.Vertices)
+            {
+                vertices.Add(v.X);
+                vertices.Add(v.Y);
+                vertices.Add(v.Z);
+            }
+        }
+
+        public static void processTextCoord(Assimp.Mesh mesh, List<float> textCoord)
+        {
+            if (!mesh.HasTextureCoords(0)) return;
+            foreach (var tc in mesh.TextureCoordinateChannels[0])
+            {
+                textCoord.Add(tc.X);
+                textCoord.Add(1-tc.Y);
+            }
+        }
+
+        public static void processNormals(Assimp.Mesh mesh, List<float> normals)
+        {
+            foreach (var n in mesh.Normals)
+            {
+                normals.Add(n.X);
+                normals.Add(n.Y);
+                normals.Add(n.Z);
+            }
+        }
+
+        public static void processIndices(Assimp.Mesh mesh, List<int> indices)
+        {
+            foreach (var f in mesh.Faces)
+            {
+               foreach(var ind in f.Indices)
+               {
+                    indices.Add(ind);
+               }
+            }
+        }
+
+        public static Mesh processMesh(Assimp.Mesh mesh)
+        {
+            List<float> positions = new List<float>();
+            List<float> textures = new List<float>();
+            List<float> normals = new List<float>();
+            //List<float> tangents = new List<float>();
+            //List<float> bitangens = new List<float>();
+            List<int> indices = new List<int>();
+
+            processIndices(mesh, indices);
+            processNormals(mesh, normals);
+            processVertices(mesh, positions);
+            processTextCoord(mesh, textures);
+
+
+            return new Mesh(positions, textures, normals, indices);
+        }
+    }
+
+    public class ModelLoader
+    {
+        public static Model LoadFromFile(string filePath, PostProcessSteps ppSteps, params PropertyConfig[] configs)
+        {
+            if(!File.Exists(filePath))
+                return null;
+
+            AssimpContext importer = new AssimpContext();
+            if(configs != null)
+            {
+                foreach(PropertyConfig config in configs)
+                    importer.SetConfig(config);
+            }
+
+            Scene scene = importer.ImportFile(filePath, ppSteps);
+            if(scene == null)
+                return null;
+
+            var m = Mesh.processMesh(scene.Meshes[0]);
+            var rm = new Model();
+            rm.meshes.Add(m);
+            return rm;
+        }
+    }
+}
